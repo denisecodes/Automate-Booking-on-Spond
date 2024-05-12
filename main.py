81f387cadb84ce8798bedf19cd6bc7936e2e7b2c
denisecodes <denise.chan112@gmail.com>
2024-05-12 22:17:29 +0100

    update script with new selenium syntax and using id to find spond dates

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import time
 import datetime as dt
 import os
 from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
@@ -31,39 +34,45 @@ my_password = os.environ.get("MY_PASSWORD")
 
 #Login to spond
 driver.get(spond_login)
-time.sleep(2)
-email_field = driver.find_element(by="name", value="emailOrPhoneNumber")
+time.sleep(1)
+email_field = driver.find_element(By.NAME, "emailOrPhoneNumber")
 email_field.send_keys(my_email)
-password_field = driver.find_element(by="name", value="password")
+password_field = driver.find_element(By.NAME, "password")
 password_field.send_keys(my_password)
-submit = driver.find_element(by="xpath", value="/html/body/root/app-login/div/div[2]/section[1]/div[2]/form/spinner-button/button")
+submit = driver.find_element(By.CLASS_NAME, "spinner-button")
 submit.click()
-time.sleep(2)
 
 #Get today's date
 today = dt.datetime.today().date()
 seven_days = dt.timedelta(days=7)
 one_week_from_today = (today + seven_days).strftime(f"%A, %-d %b at 11:00")
 print(one_week_from_today)
+time.sleep(1)
+
+#Wait until badminton dates are loaded
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="event_card_start_time"]'))
+)
 
 #Get dates for badminton sessions
-spond_dates = driver.find_elements(by="css selector", value=".spondCardstyled__EventStartTime-sc-1adg5oi-22")
+spond_dates = driver.find_elements(By.XPATH, '//*[@id="event_card_start_time"]')
 #Retrieves text of badminton dates, storing it into a list
-badminton_dates = [spond_date.get_attribute('innerHTML') for spond_date in spond_dates]
+badminton_dates = [spond_date.text for spond_date in spond_dates]
 print(badminton_dates)
 
 # Check if there is a session that is one week from today and book session
 for badminton_date in badminton_dates:
     if one_week_from_today == badminton_date:
-        print("There is a session in exactly two weeks from today")
+        print("There is a session in exactly one week from today")
         #Find an element by it's text value using xpath
         #Click the date of the session one week from today
-        date_button = driver.find_element(by="xpath", value=f"//*[contains(text(), '{badminton_date}')]")
+        xpath_query = f"//*[@id='event_card_start_time' and contains(text(), '{badminton_date}')]"
+        date_button = driver.find_element(By.XPATH, xpath_query)
         date_button.click()
         time.sleep(1)
         while True:
             try:
-                accept_button = driver.find_element(by="name", value="accept-button")
+                accept_button = driver.find_element(By.NAME, "accept-button")
             except NoSuchElementException:
                 print("Accept button not found")
             else:
